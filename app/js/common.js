0d430fef69dd34ad3cc8bad3290c5fc9170e033a
Sergey Kozlov <s.kozlov@demis.ru>
2018-02-20 16:00:12 +0300

    init commit

---
+++ app/js/common.js
@@ -0,0 +1,17 @@
+$(function(){
+	
+	$('.inputmask').inputmask('+7(999)-999-99-99');
+
+	$('.header__seearch').click(function(){
+		$(this).toggleClass('active');
+		$('.header__search').toggle(300);
+	});
+
+	$('.slider__main__wrapper').owlCarousel({
+		items: 1,
+		loop: true,
+		nav: true,
+		navText: ['<span class="icon-arr-left"></span>','<span class="icon-arr-right"></span>']
+	});
+
+});
